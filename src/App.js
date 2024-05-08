3014825470a145880a97b121ab7d042fd6e1eddd
Linda Han <hanlinda0903@gmail.com>
2024-05-07 19:10:09 -0700

    worked on main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './assets/linda-logo.png';
+import Introduction from './Introduction';
 
 function App() {
   return (
     <div className="App">
       <img src={logo} alt="linda-logo" className="logo" />
-      <p>pssst</p>
+      < Introduction />
     </div>
   );
 }
---
+++ src/Introduction.js
@@ -0,0 +1,7 @@
+const Introduction = () => {
+  return ( 
+    <h1 className="intro-title">Hi, I'm Linda</h1>
+  );
+}
+ 
+export default Introduction;
