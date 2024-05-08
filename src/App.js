45e6abb125ebbb3851e81718091a4600be11a830
Linda Han <hanlinda0903@gmail.com>
2024-05-08 14:08:22 -0700

    view resume button finished

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} alt="linda-logo" className="logo" />
         < Introduction />
         < Navbar />
-        <h3 className="message">🚧 This page is still under construction 🚧</h3>
+        <h3 className="message">🚧 This page is currently under construction 🚧</h3>
         < Icons />
     </div>
   );
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,15 @@
+import resume from './assets/Linda_Han_Resume.pdf';
+
 const Navbar = () => {
+  const openResume = () => {
+    window.open(resume, '_blank');
+  };
+
   return ( 
     <div className="navbar">
-      <button type="button" class="btn btn-block">Work Experiences</button>
-      <button type="button" class="btn btn-block">My Projects</button>
-      <button type="button" class="btn btn-block">Photos</button>
+      <button type="button" className="btn btn-block">Work Experience</button>
+      <button type="button" className="btn btn-block">My Projects</button>
+      <button type="button" className="btn btn-block" onClick={openResume}>Resume</button>
     </div>
   );
 }
